0a5fb22d322fe2a14208eb5142ded8e11593c382
Simen Johannessen <simenlomaas@gmail.com>
2015-03-31 20:08:24 +0200

    Added two first view controllers

--- TheCandyChallenge/InviteFriendsViewController.swift
+++ TheCandyChallenge/InviteFriendsViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  InviteFriendsViewController.swift
 //  TheCandyChallenge
 //
-//  Created by Simen Johannessen on 25/03/15.
+//  Created by Simen Johannessen on 31/03/15.
 //  Copyright (c) 2015 Simen Lomås Johannessen. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class InviteFriendsViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var searchField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+        
     }
-
-
 }
-
---
+++ TheCandyChallenge/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheCandyChallenge
+//
+//  Created by Simen Johannessen on 25/03/15.
+//  Copyright (c) 2015 Simen Lomås Johannessen. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

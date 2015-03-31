35f41a72946610248585bc88d7b5c1ff13215322
Simen Johannessen <simenlomaas@gmail.com>
2015-03-31 23:59:43 +0200

    ContainerViewcontroller added

---
+++ TheCandyChallenge/ContainerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerViewController.swift
+//  TheCandyChallenge
+//
+//  Created by Simen Johannessen on 31/03/15.
+//  Copyright (c) 2015 Simen Lomås Johannessen. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    var controllers : [UIViewController] = [];
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

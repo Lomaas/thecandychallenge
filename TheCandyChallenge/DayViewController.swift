c3ce48b3a5a8cd334fb74a73f878701cd8b5d7af
Simen Johannessen <simenlomaas@gmail.com>
2015-04-11 21:02:35 +0200

    updates

--- TheCandyChallenge/DayViewController.swift
+++ TheCandyChallenge/DayViewController.swift
@@ -10,18 +10,74 @@ import Foundation
 import UIKit
 
 class DayViewController: UIViewController {
-    
+    @IBOutlet weak var interactionButton: UIButton!
     @IBOutlet weak var headerImage: UIImageView!
-
     @IBOutlet weak var mainText: UILabel!
     
     @IBAction func interactionButtonPressed(sender: UIButton) {
-        
+        println("Interaction button pressed")
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        isEarlyDay() ? showEarlyDayScreen() : showLateDayScreen()
+    }
+
+    func isEarlyDay() -> Bool {
+        let components = NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitHour | NSCalendarUnit.CalendarUnitSecond | NSCalendarUnit.CalendarUnitMinute
+        let date = NSCalendar.currentCalendar().components(components, fromDate: NSDate())
         
+        println("IS easrly day view: \(date.hour)")
+        
+        if date.hour > 21 {
+            return false
+        }
+        return true
     }
     
+    func showEarlyDayScreen() {
+        interactionButton.setTitle(NSLocalizedString("Dismiss", comment: ""), forState: UIControlState.Normal)
+        
+        let dayOfWeek: Int = getWeekDay(NSDate())
+        
+        switch dayOfWeek {
+        case Days.monday:
+            println("Monday")
+            headerImage.image = UIImage(named: "monday")
+            mainText.text = NSLocalizedString("Its monday baby! Stay healthy, avoid spiking blood sugar, stay away from the candy, and you will be fine.", comment: "")
+        case Days.saturday:
+            println("Saturday")
+            headerImage.image = UIImage(named: "saturday")
+            mainText.text = NSLocalizedString("Today you´ll MASSIVE cravings for candy! Can you stay away?", comment: "")
+        default:
+            println("Did not match any days \(dayOfWeek)")
+        }
+    }
+    
+    func showLateDayScreen() {
+        let dayOfWeek: Int = getWeekDay(NSDate())
+        
+        switch dayOfWeek {
+        case Days.monday:
+            println("Monday")
+            headerImage.image = UIImage(named: "monday")
+            mainText.text = NSLocalizedString("An easy day huh? Monday isnt a problem. You just had weekend", comment: "")
+        case Days.saturday:
+            println("Saturday")
+            headerImage.image = UIImage(named: "saturday")
+            mainText.text = NSLocalizedString("I guess you had massive CRAVINGS today I congratulate you if you made it. By the way, you owe me if you are lying", comment: "")
+        default:
+            println("Did not match any days \(dayOfWeek)")
+        }
+    }
+    
+    func shouldShowEarlyDayScreen() -> Bool {
+        return true
+    }
+    
+    func getWeekDay(date: NSDate) -> Int {
+        let myComponents = NSCalendar.currentCalendar().component(NSCalendarUnit.WeekdayCalendarUnit, fromDate: date)
+        return myComponents
+    }
 }
--- TheCandyChallenge/ProgressViewController.swift
+++ TheCandyChallenge/ProgressViewController.swift
@@ -9,7 +9,6 @@
 import Foundation
 import UIKit
 
-
 struct Days {
     static let monday = 2
     static let tuesday = 3
@@ -21,11 +20,7 @@ struct Days {
 }
 
 class ProgressViewController: UIViewController {
-    var challenge: PFObject? {
-        didSet {
-            println("Didset progress view")
-        }
-    }
+    var challenge: PFObject?
     
     @IBOutlet weak var dayViewContainer: UIView!
     @IBOutlet weak var moneySaved: UILabel!
@@ -35,10 +30,27 @@ class ProgressViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        HealthKitService().authorizeHealthKit { (authorized, error) -> Void in
+            if authorized {
+                println("HealthKit authorization received.")
+                HealthKitService().readMostRecentSample(, completion: { (sample, error) -> Void in
+                    
+                })
+            }
+            else
+            {
+                println("HealthKit authorization denied!")
+                if error != nil {
+                    println("\(error)")
+                }
+            }
+        }
+        
         UserChallengeService.getMyChallengeFromLocalStorage({ (userChallenge: PFObject) -> Void in
             self.challenge = userChallenge
             var f: String = self.challenge!.objectId
             println("Has challenge with objectID: \(f)")
+            userChallenge.pinInBackground()
             self.updateScreen()
             
         }, errorHandler: { () -> Void in
@@ -49,43 +61,16 @@ class ProgressViewController: UIViewController {
         })
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     func updateScreen() {
-        shouldShowEarlyDayScreen() ? showEarlyDayScreen() : showLateDayScreen()
+        if shouldShowDayView() { showDayView() }
         
-        calories.text = challenge?["calories"] as? String
+//        calories.text = challenge?["calories"] as? String
         daysLabel.text = getDaysSinceStarted(challenge!.createdAt!)
-        moneySaved.text = challenge?["moneySaved"] as? String
-        
-        
-    }
-    
-    func showEarlyDayScreen() {
-        dayViewContainer.hidden = false
-        dayViewContainer.frame = self.view.frame
-        
-        let dayOfWeek: Int = getWeekDay(NSDate())
-
-        switch dayOfWeek {
-        case Days.monday:
-            println("Monday")
-        case Days.saturday:
-            println("Saturday")
-        default:
-            println("Did not match any days \(dayOfWeek)")
-        }
-    }
-    
-    func showLateDayScreen() {
-        
-    }
-    
-    func shouldShowEarlyDayScreen() -> Bool {
-        return true
-    }
-    
-    func getWeekDay(date: NSDate) -> Int {
-        let myComponents = NSCalendar.currentCalendar().component(NSCalendarUnit.WeekdayCalendarUnit, fromDate: date)
-        return myComponents
+//        moneySaved.text = challenge?["moneySaved"] as? String
     }
     
     func getDaysSinceStarted(date: NSDate) -> String {
@@ -93,27 +78,21 @@ class ProgressViewController: UIViewController {
         let date = NSCalendar.currentCalendar().components(components, fromDate: date, toDate: NSDate(), options: nil)
         return "\(date.day) days  \(date.hour) hours \(date.minute) minutes"
     }
-}
+    
+    func shouldShowDayView() -> Bool {
+        let defaults = NSUserDefaults.standardUserDefaults()
 
-public extension NSDate {
-    func xDays(x:Int) -> NSDate {
-        return NSCalendar.currentCalendar().dateByAddingUnit(.CalendarUnitDay, value: x, toDate: self, options: nil)!
+        if defaults.objectForKey("seenTodayView") != nil {
+            defaults.setObject("yes", forKey: "seenTodayView")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            return true
+        }
+        else {
+            return false
+        }
     }
-    func xWeeks(x:Int) -> NSDate {
-        return NSCalendar.currentCalendar().dateByAddingUnit(.CalendarUnitWeekOfYear, value: x, toDate: self, options: nil)!
-    }
-    var hoursFromToday: Int{
-        return NSCalendar.currentCalendar().components(.CalendarUnitHour, fromDate: self, toDate: NSDate(), options: nil).hour
-    }
-    var daysFromToday: Int{
-        return NSCalendar.currentCalendar().components(.CalendarUnitDay, fromDate: self, toDate: NSDate(), options: nil).hour
-    }
-    var weeksFromToday: Int{
-        return NSCalendar.currentCalendar().components(.CalendarUnitWeekOfYear, fromDate: self, toDate: NSDate(), options: nil).weekOfYear
-    }
-    var relativeDateString: String {
-        if weeksFromToday > 0 { return weeksFromToday > 1 ? "\(weeksFromToday) weeks and \(hoursFromToday) hours" : "\(weeksFromToday) week and \(hoursFromToday) hours"   }
-        if hoursFromToday > 0 { return hoursFromToday > 1 ? "\(hoursFromToday) hours" : "\(hoursFromToday) hour"   }
-        return ""
+    
+    func showDayView() {
+        performSegueWithIdentifier("dayViewController", sender: self)
     }
 }

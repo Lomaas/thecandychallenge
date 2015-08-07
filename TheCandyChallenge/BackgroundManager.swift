047b30e2bb62d4e85a54c3cdf56ef62ffa85b0bc
Simen Johannessen <simenlomaas@gmail.com>
2015-08-08 00:06:38 +0200

    changes to notifications work

---
+++ TheCandyChallenge/BackgroundManager.swift
@@ -0,0 +1,9 @@
+//
+//  BackgroundManager.swift
+//  TheCandyChallenge
+//
+//  Created by Simen Johannessen on 07/08/15.
+//  Copyright (c) 2015 Simen Lomås Johannessen. All rights reserved.
+//
+
+import Foundation
